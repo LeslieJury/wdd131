145f4464721014f51c85e301341375cece8e7564
Leslie <129573291+LeslieJury@users.noreply.github.com>
2026-04-27 15:00:08 -0600

    dom basics

---
+++ ponder/dom-basics/dom-basics.js
@@ -0,0 +1,21 @@
+let title = document.querySelector('h1');
+
+console.log(title);
+
+title.textContent = "Web Page Components";
+
+const topics = document.querySelector('#topics');
+
+topics.style.color = 'red';
+const content = document.getElementById('content');
+
+content.style.backgroundColor = 'lightblue';
+
+let list = document.querySelector(".list");
+
+list.style.border = "3px solid black";
+
+const paragraph = document.querySelector('p');
+
+paragraph.style.fontSize = "3em";
+
