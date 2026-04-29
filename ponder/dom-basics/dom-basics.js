0561118d4f9a68e99a41dc1ea4aa5fcd2aff6192
Leslie <129573291+LeslieJury@users.noreply.github.com>
2026-04-29 14:47:10 -0600

    Ponder: DOM

--- ponder/dom-basics/dom-basics.js
+++ ponder/dom-basics/dom-basics.js
@@ -6,7 +6,7 @@ title.textContent = "Web Page Components";
 
 const topics = document.querySelector('#topics');
 
-topics.style.color = 'red';
+//topics.style.color = 'red';
 const content = document.getElementById('content');
 
 content.style.backgroundColor = 'lightblue';
@@ -19,3 +19,24 @@ const paragraph = document.querySelector('p');
 
 paragraph.style.fontSize = "3em";
 
+paragraph.classList.add('background');
+
+const image = document.querySelector('img')
+image.setAttribute('src', 'images/new_logo.webp');
+
+const dropdown = document.querySelector('#webdevlist');
+
+const html = document.querySelector('#html');
+const css = document.querySelector('#css');
+const js = document.querySelector('#js');
+
+
+dropdown.addEventListener('change', function(){
+    html.style.color = "black";
+    css.style.color = "black";
+    js.style.color = "black";
+
+    let codeValue = dropdown.value;
+    console.log(codeValue);
+    document.getElementById(codeValue).style.color = 'red';
+});
